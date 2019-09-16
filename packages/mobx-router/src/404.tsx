20a8c47e01dbdec1c5ff214131f55a1bf67915c5
Gevik Babakhani <gevik@blendjs.com>
2019-09-16 21:29:01 +0200

    Added comment

--- packages/mobx-router/src/404.tsx
+++ packages/mobx-router/src/404.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 
 /**
- * Provides a basic 404 page
+ * Provides a basic 404 page.
  *
  * @export
  * @returns
  */
 export function NotFound404() {
-    return <div>Not found!</div>
+    return <div>Not found!</div>;
 }
