33833e701623f7ba3bb14caf886693b3d5eb12f4
Gevik Babakhani <gevikb@gmail.com>
2019-09-16 18:14:37 +0200

    Added docs

--- packages/mobx-router/src/store.ts
+++ packages/mobx-router/src/store.ts
@@ -1,8 +1,10 @@
 import { deepCopy } from "@blendsdk/stdlib/dist/deepcopy";
 import { IDictionary } from "@blendsdk/stdlib/dist/types";
 import { wrapInArray } from "@blendsdk/stdlib/dist/wrapInArray";
+import { apply } from "@blendsdk/stdlib/dist/apply";
 import { History } from "history";
-import { observable, reaction } from "mobx";
+import { observable, reaction, action } from "mobx";
+import { NotFound404 } from "./404"
 import pathToRegexp, { Key, PathFunction } from "path-to-regexp";
 
 export type TRouterComponent = (...params: any[]) => any;
@@ -44,7 +46,25 @@ export interface IRoute {
     defaults?: IDictionary;
 }
 
-type TConfiguredRoute = [RegExp, Key[], PathFunction<any>, TRouterComponent, IDictionary];
+/**
+ * Interface describing a parsed IRoute object
+ *
+ * @interface IParsedRoute
+ */
+interface IParsedRoute {
+    matcher: RegExp,
+    matcherKeys: Key[],
+    toPath: PathFunction<any>
+    component: TRouterComponent,
+    defaults: IDictionary
+}
+
+/**
+ * Interface describing a matched component
+ *
+ * @export
+ * @interface IMatchedComponent
+ */
 export interface IMatchedComponent {
     component: TRouterComponent;
     params: IDictionary;
@@ -73,7 +93,7 @@ export class RouterStore {
      * @memberof RouterStore
      */
     protected urlBuilderCache: {
-        [name: string]: TConfiguredRoute
+        [name: string]: IParsedRoute
     } = {};
 
     /**
@@ -84,7 +104,7 @@ export class RouterStore {
      * @type {(History<any> | undefined)}
      * @memberof RouterStore
      */
-    protected history: History<any> | undefined;
+    protected history?: History<any>;
 
     /**
      * Check if the urlBuilderCache is already initialized.
@@ -121,24 +141,70 @@ export class RouterStore {
         this.location = history.location.pathname;
     }
 
+    /**
+     * Initializes a default 404 route
+     *
+     * @protected
+     * @memberof RouterStore
+     */
+    protected init404() {
+        const me = this;
+        me.initRoute({
+            name: '404',
+            path: '/404',
+            component: NotFound404
+        })
+    }
+
+    /**
+     * Initialoizes a single route
+     *
+     * @protected
+     * @param {IRoute} route
+     * @memberof RouterStore
+     */
+    protected initRoute(route: IRoute) {
+        const me = this;
+        route.name = route.name || route.path;
+        if (!me.urlBuilderCache[route.name]) {
+            const matcherKeys: Key[] = [],
+                matcher = pathToRegexp(route.path, matcherKeys);
+            me.urlBuilderCache[route.name] = {
+                matcher,
+                matcherKeys,
+                toPath: pathToRegexp.compile(route.path),
+                component: route.component,
+                defaults: route.defaults
+            }
+        }
+    }
+
+    /**
+     * Initializes the routes
+     *
+     * @param {IRoute[]} routes
+     * @memberof RouterStore
+     */
     public initRoutes(routes: IRoute[]) {
-        let me = this;
+        const me = this;
         if (!me.initialized) {
             wrapInArray<IRoute>(routes || []).forEach(route => {
-                // make sure we always have a name
-                route.name = route.name || route.path;
-                if (!me.urlBuilderCache[route.name]) {
-                    const matcherKeys: Key[] = [],
-                        matcher = pathToRegexp(route.path, matcherKeys);
-
-                    me.urlBuilderCache[route.name] = [
-                        matcher,
-                        matcherKeys,
-                        pathToRegexp.compile(route.path),
-                        route.component, route.defaults || {}];
-                }
+                me.initRoute(route)
             });
+            me.init404();
             me.initialized = true;
+        }
+    }
+
+    @action
+    public go(pathName: string, params?: IDictionary) {
+        const me = this,
+            route = me.urlBuilderCache[pathName] || null;
+        if (route) {
+            const { toPath, defaults } = route;
+            me.history.push(toPath(apply(params || {}, defaults)));
+        } else {
+            me.history.push(pathName)
         }
     }
 
@@ -150,27 +216,24 @@ export class RouterStore {
      */
     get MatchedComponent(): IMatchedComponent {
         const me = this;
-        let result: IMatchedComponent = { component: null, params: null } as any;
-        (Object.values(me.urlBuilderCache) || []).forEach((item: TConfiguredRoute) => {
-            if (!result) {
-                const [
-                    matcher,
-                    matcherKeys,
-                    toPath,
-                    component,
-                    defaults] = item;
-
-                if (matcher.test(me.location)) {
-                    // we have matched the route
-                    const matched = (matcher.exec(me.location) || []).splice(1);
-                    const params = deepCopy(defaults || {});
-                    matcherKeys.forEach((key, index) => {
-                        params[key.name] = matched[index] || undefined;
-                    });
-                    result = { component, params };
-                }
+        for(let index in me.urlBuilderCache) {
+            const item = me.urlBuilderCache[index];
+            const {
+                matcher,
+                matcherKeys,
+                component,
+                defaults } = item;
+            if (matcher.test(me.location)) {
+                // we have matched the route
+                const matched = (matcher.exec(me.location) || []).splice(1);
+                const params = deepCopy(defaults || {});
+                matcherKeys.forEach((key, index) => {
+                    params[key.name] = matched[index] || undefined;
+                });
+                return { component, params };
             }
-        });
-        return result;
+
+        }
+        return { component: me.urlBuilderCache['404'].component, params: {} }
     }
 }
