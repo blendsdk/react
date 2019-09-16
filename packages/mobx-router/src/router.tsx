d583e3cf816ff4ead95b98e46515efcff76701b1
Gevik Babakhani <gevik@blendjs.com>
2019-09-16 21:29:12 +0200

    Make linter happy

--- packages/mobx-router/src/router.tsx
+++ packages/mobx-router/src/router.tsx
@@ -1,13 +1,13 @@
 import { observer } from "mobx-react";
-import React, { useEffect, ReactNode } from "react";
-import { useRouter, routerProvider } from "./context";
+import React, { ReactNode, useEffect } from "react";
+import { routerProvider, useRouter } from "./context";
 import { IRoute } from "./store";
 
-export interface RouterProps {
+export interface IRouterProps {
     routes: IRoute[];
 }
 
-export const Router = observer(({ routes }: RouterProps) => {
+export const Router = observer(({ routes }: IRouterProps) => {
     const routerStore = useRouter();
     routerStore.initRoutes(routes);
 
@@ -15,7 +15,7 @@ export const Router = observer(({ routes }: RouterProps) => {
         // no-op
     }, [routerStore.location]);
 
-    const {component, params} = routerStore.MatchedComponent;
+    const { component, params } = routerStore.MatchedComponent;
 
     // Why ?
     const Component = component;
@@ -24,14 +24,14 @@ export const Router = observer(({ routes }: RouterProps) => {
 });
 
 export const Dummy = observer(() => {
-    return <div>Dummy</div>
+    return <div>Dummy</div>;
 });
 
-export interface RouterProviderProps {
-    children?: ReactNode
+export interface IRouterProviderProps {
+    children?: ReactNode;
 }
 
-export const RouterProvider = ({children}:RouterProviderProps) => {
+export const RouterProvider = ({ children }: IRouterProviderProps) => {
     const store = React.useContext(routerProvider);
-    return <routerProvider.Provider value={store}>{children}</routerProvider.Provider>
-}
+    return <routerProvider.Provider value={store}>{children}</routerProvider.Provider>;
+};
